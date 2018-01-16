2cc88ab78c71ed351a3244c55227e4ab74c6555d
Derek Xu <derekxu.uhs@gmail.com>
2018-01-16 09:51:25 -0500

    Initial commit

---
+++ app/src/main/java/dlhxu/com/mealprep/MainActivity.java
@@ -0,0 +1,14 @@
+package dlhxu.com.mealprep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//  TODO create a New Meal class for when they want to enter a new type of meal
+
+public class MainActivity extends AppCompatActivity {
+    // TODO flesh out the main activity and main layout
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
